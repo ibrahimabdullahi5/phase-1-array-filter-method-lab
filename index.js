188d15323e4abd587b4a97098a2d19bb850649f3
ibrahimabdullahi5 <ibrahimabdullahibare@DESKTOP-0DEOD38.localdomain>
2024-05-27 00:18:39 -0900

    complete array-filter lab

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
-// Code your solution here
+function findMatching(drivers, name) {
+    return drivers.filter(driver => driver.toLowerCase() === name.toLowerCase());
+  }
+  
+  const drivers = ['ibro', 'stopper', 'shola', 'Annette', 'Sahara', 'abdi'];
+  console.log(findMatching(drivers, 'ibro')); 
+  console.log(findMatching(drivers, 'Stopper')); 
+  console.log(findMatching(drivers, 'sahara')); 
+
+  function fuzzyMatch(drivers, letters) {
+    return drivers.filter(driver => driver.toLowerCase().startsWith(letters.toLowerCase()));
+  }
+  
+  let drivers1 = ['sacdiyo', 'binto', 'Salah', 'Anisa', 'Sahara', 'moha'];
+  console.log(fuzzyMatch(drivers, 'binto')); 
+  console.log(fuzzyMatch(drivers, 'sacdiyo')); 
+  
+  
+  function matchName(drivers, name) {
+    return drivers.filter(driver => driver.name.toLowerCase() === name.toLowerCase());
+  }
+  
+  const drivers2 = ['sacdiyo', 'binto', 'Sumeyo', 'Anisa', 'Sahara', 'moha'];   
+  
+  console.log(matchName(drivers, 'hassan')); 
+  console.log(matchName(drivers, 'Sumeyo')); 
+  
